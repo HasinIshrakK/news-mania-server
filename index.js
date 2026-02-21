f239c0f87440b2a947ee52e3bed50a004aef8fe8
HasinIshrakK <hasinishrakk@gmail.com>
2026-02-21 11:36:35 +0600

    feat: setup express server and connect to MongoDB

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db;
+let articlesCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("newsDB");
+    articlesCollection = db.collection("articles");
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("DB Connection Error:", err.message);
+  }
+}
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
